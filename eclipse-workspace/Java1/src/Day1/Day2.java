b13422068c2e62cce1f87ebd3367eeabc9696b74
u <u@DESKTOP-E0GIBTF>
2025-10-14 18:49:05 +0530

    Execute if else statement

--- eclipse-workspace/Java1/src/Day1/Day2.java
+++ eclipse-workspace/Java1/src/Day1/Day2.java
@@ -1,10 +1,32 @@
 package Day1;
 
+import java.util.Scanner;
+
 public class Day2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//Conditional Statements
+//		if 
+//		if else
+//		if else if
+//		nested if 	
+		
+		//if
+		
+		int originalpassword = 1234;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter your password");
+		int yourpassword = sc.nextInt();
+		if(yourpassword==originalpassword) {
+			System.out.println("Password is right");
+		}else {
+			System.out.println("Password is wrong");
+		}
+		
+		
+		
 	}
 
 }
