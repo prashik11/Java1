7ff61525b8ffcd0a9b1362eb5e8e8e931530c2a0
u <u@DESKTOP-E0GIBTF>
2025-10-15 19:00:43 +0530

    Nested if

--- eclipse-workspace/Java1/src/Day1/Day2.java
+++ eclipse-workspace/Java1/src/Day1/Day2.java
@@ -12,20 +12,35 @@ public class Day2 {
 //		if else if
 //		nested if 	
 		
-		//if
+		//if else
 		
-		int originalpassword = 1234;
+//		int originalpassword = 1234;
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("Enter your password");
+//		int yourpassword = sc.nextInt();
+//		if(yourpassword==originalpassword) {
+//			System.out.println("Password is right");
+//		}else {
+//			System.out.println("Password is wrong");
+//		}
+		
+		
+		//Calculator Using Nested IF 
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number = ");
+		int num1 = sc.nextInt();
+		System.out.println("Enter a second number");
+		int num2 = sc.nextInt();
 		
-		System.out.println("Enter your password");
-		int yourpassword = sc.nextInt();
-		if(yourpassword==originalpassword) {
-			System.out.println("Password is right");
-		}else {
-			System.out.println("Password is wrong");
+		System.out.println("Which operation do you want to perform +, -, *, / ");
+		char op= sc.next().charAt(0);
+		
+		if(op=='+') {
+			int sum = num1+num2;
+			System.out.println(num1 + "+" + num2 +"="+ sum);
+			
 		}
-		
-		
 		
 	}
 
