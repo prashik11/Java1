c4ca990d757d9cc97e806ca7de6cf93c0922c0e1
u <u@DESKTOP-E0GIBTF>
2025-10-31 15:06:28 +0530

    If else statement

---
+++ eclipse-workspace/Java1/src/Day1/Day3.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class Day3 {
+
+	public static void main(String[] args) {
+		
+		// for loop is use for multiple conditions and if the no of iteration is fixed, it is recommended to use for loop.
+		// print a number 1 to 10
+		
+		for(int a = 1 ; a<=10 ; a++) {
+			System.out.println(a);
+		}
+		
+		//Print 9 table
+		for(int b = 9 ; b<=90 ; b=b+9) {
+		    System.out.println(b);	
+		}
+	}
+
+}
